d488a334d3ac3720231115553b5046835a3964c1
sherif <sherifshams1992@gmail.com>
2024-04-26 13:04:39 +0200

    Auth is Needed

---
+++ RaterApp/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Meal, Rating
+
+
+class MealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meal
+        fields = ('id', 'title', 'description', 'no_of_ratings', 'avg_rating')
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('id', 'stars', 'user', 'meal')

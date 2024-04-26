08044f4375cbefec0017a170a5529656bd72e2e1
sherif <sherifshams1992@gmail.com>
2024-04-26 20:37:07 +0200

    Auth Complete, Wait fot Deploy Heroku

--- RaterApp/urls.py
+++ RaterApp/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
-from .views import MealViewSet, RatingViewSet
+from .views import MealViewSet, RatingViewSet, UserViewSet
+
+from rest_framework.authtoken.views import ObtainAuthToken
 
 router = routers.DefaultRouter()
-router.register('meals', MealViewSet)
-router.register('ratings', RatingViewSet)
+router.register('meals', viewset=MealViewSet)
+router.register('ratings', viewset=RatingViewSet)
+router.register('users', UserViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', ObtainAuthToken.as_view()),
+
 ]
